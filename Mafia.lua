6f363142910943ee4cc8b6989d759d7d3567432b
Amped Scripts <52733865+AmpedScripts@users.noreply.github.com>
2022-01-11 09:22:08 +0100

    Gui

---
+++ Mafia.lua
@@ -0,0 +1,7 @@
+local DiscordLib = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/discord%20lib.txt")()
+
+local win = DiscordLib:Window("Mafia Cheats")
+
+local serv = win:Server("Mafia Cheats", "")
+
+local btns = serv:Channel("Buttons")
